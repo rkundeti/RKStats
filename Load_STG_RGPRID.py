d0cc6a51bbc6e6dbf6f066286246b5dc665015ac
Ramana Kundeti <rkundeti98@gmail.com>
2018-12-19 22:20:25 -0500

    Add files via upload

---
+++ Load_STG_RGPRID.py
@@ -0,0 +1,12 @@
+import csv, pypyodbc as pyodbc
+conn = pyodbc.connect("DRIVER=NetezzaSQL;SERVER=ibmnetqa;PORT=5480;DATABASE=BMIS_STG;UID=BMIS_ETL_USR;PWD=ptmd3233;")
+cursor = conn.cursor()
+cursor.execute("TRUNCATE TABLE STG_CDM_RGPRID_TEST")
+with open('US_MDM_RGPRID_201600429.csv','r') as fin: # `with` statement available in 2.5+
+    # csv.DictReader uses first line in file for column headings by default
+    dr = csv.DictReader(fin) # comma is default delimiter
+    to_db = [(i['RGPRID'], i['AGECID']) for i in dr]
+
+cursor.executemany("INSERT INTO STG_CDM_RGPRID_TEST (RGPRID , AGECID) VALUES (?, ?);", to_db)
+conn.commit()
+
